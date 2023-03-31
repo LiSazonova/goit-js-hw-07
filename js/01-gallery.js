1ed3df69c4d8bacce93620fe171a135106486e32
LiSazonova <lilissazonova@gmail.com>
2023-03-31 21:26:37 +0300

    add gallery 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,41 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const addGalleryItems = galleryCreateMarkup(galleryItems);
+
+galleryEl.innerHTML = addGalleryItems;
+galleryEl.addEventListener('click', onGalleryElClick);
+
+function galleryCreateMarkup(items) {
+    return items
+        .map(({ preview, original, description }) =>
+        `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `,)
+        .join('');
+}
+
+function onGalleryElClick(evt) {
+    evt.preventDefault();
+    console.log(evt.target);
+
+    const imgSource = evt.target.dataset.source;
+    console.log(imgSource);
+    const instance = basicLightbox.create(
+    `
+    <img src="${imgSource}" width="800" height="600">
+`)
+    instance.show();
+}

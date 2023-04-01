375475bb1f07612d6a6509bd3347a4986149e8f0
LiSazonova <lilissazonova@gmail.com>
2023-04-01 11:16:51 +0300

    bug fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -15,9 +15,14 @@ function galleryCreateMarkup(items) {
         .map(({ preview, original, description }) =>
         `
     <li class="gallery__item">
-   <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" alt="${description}" />
-   </a>
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
 </li>
     `)
         .join('');
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -16,16 +16,11 @@ function galleryCreateMarkup(items) {
         .map(({ preview, original, description }) =>
         `
     <li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
 </li>
-    `,)
+    `)
         .join('');
 }
 
